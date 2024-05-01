64119ecf78cd5b21ad81c4396991cd0d803c3609
Ross McFarland <rwmcfa1@gmail.com>
2024-05-01 16:25:27 -0700

    rigid back porous absorbers

--- acoustics/constants.py
+++ acoustics/constants.py
@@ -5,3 +5,11 @@
 speed_of_sound = 343
 beta = 1.5 / 1.7
 rho = 1.1839
+
+gas_constant = 287.05  # J Kg-1 °K-1
+one_atmosphere = 101325  # Pa
+air_density_at_0 = 1.293  # Kg m-3, Kinsler & Frey, Appendix A10, Table (c)
+gamma = 1.402  # Kinsler & Frey, Appendix A10, Table (c)
+air_viscosity = 0.00001850  # m2s-1
+air_thermal_conductivity = 0.0241
+air_specific_heat = 1.01
