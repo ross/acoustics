ad431448398aaab54caf55f9c366636e5e6578c7
Ross McFarland <rwmcfa1@gmail.com>
2024-05-15 16:02:50 -0700

    2d qrd

--- acoustics/constants.py
+++ acoustics/constants.py
@@ -2,6 +2,8 @@
 #
 #
 
+import numpy as np
+
 speed_of_sound = 343
 beta = 1.5 / 1.7
 rho = 1.1839
@@ -13,3 +15,31 @@ gamma = 1.402  # Kinsler & Frey, Appendix A10, Table (c)
 air_viscosity = 0.00001850  # m2s-1
 air_thermal_conductivity = 0.0241
 air_specific_heat = 1.01
+
+primes = np.array(
+    (
+        3,
+        5,
+        7,
+        11,
+        13,
+        17,
+        19,
+        23,
+        29,
+        31,
+        37,
+        43,
+        47,
+        53,
+        59,
+        61,
+        67,
+        71,
+        73,
+        79,
+        83,
+        89,
+        97,
+    )
+)
